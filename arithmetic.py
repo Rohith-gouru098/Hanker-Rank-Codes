060bd7a763635ae496b60ae441fe111905999d3a
Rohith-gouru098 <152318478+Rohith-gouru098@users.noreply.github.com>
2024-12-05 23:05:44 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,11 @@
+#Arithmetic Operators
+if __name__ == '__main__':
+    a = int(input("Element of a:"))
+    b = int(input("Element of b:"))
+sum_result = a + b
+difference_result = a - b
+product_result = a * b
+
+print(sum_result)
+print(difference_result)
+print(product_result)

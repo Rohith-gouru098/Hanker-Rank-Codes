4c20e66f6c74e1e2dd9f92a06c1f923382426c18
RohithChandu Gouru <152318478+Rohith-gouru098@users.noreply.github.com>
2024-12-12 22:52:31 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,15 @@
+class Rectangle:
+  def __init__(self, length, width):
+    self.length = length
+    self.width = width
+
+  def area(self):
+    return self.length * self.width
+  pass
+class Circle:
+  def __init__(self, radius):
+    self.radius = radius
+
+  def area(self):
+    return math.pi*(self.radius **2)
+  pass
